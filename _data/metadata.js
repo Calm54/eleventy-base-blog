d273884375a5ef5d72baa66603c77581ba003035
C@lm <cmadu53@gmail.com>
2023-07-17 20:08:34 +0100

    First commit

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	url: "https://calm54.github.io/Profile-page/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my new found World of IT",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Catherine Madu",
+		email: "cmadu53@gmail.com",
+		url: "https://calm54.github.io/Profile-page/"
 	}
 }
