9bdb5ca56d2b447f06f6ad87d1413496f6f561c8
C@lm <cmadu53@gmail.com>
2023-07-23 21:41:29 +0100

    Url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
-	url: "https://calm54",
+	url: "https://github.com/Calm54/eleventy-base-blog.git",
 	language: "en",
 	description: "I am writing about my new found World of IT",
 	author: {
 		name: "Catherine Madu",
 		email: "cmadu53@gmail.com",
-		url: "https://calm54"
+		url: "https://github.com/Calm54/eleventy-base-blog.git"
 	}
 }
